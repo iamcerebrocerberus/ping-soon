207edbec6edc10366d9966c50ab94ece41da4be8
Cerebro_Cerberus <sethamek@gmail.com>
2022-02-18 22:28:06 +0000

    rm unneeded code from js file

--- App.js
+++ App.js
@@ -7,9 +7,10 @@ const errorMsg = {
     empty: "Whoops! It looks like you forgot to add your email",
     wrgFmt: "Please provide a valid email address"
 }
+
 form.onsubmit = function(e) {
     e.preventDefault();
-    console.log('working');
-    document.body.style.backgroundColor = 'red';
     
 }
+
+// not done
